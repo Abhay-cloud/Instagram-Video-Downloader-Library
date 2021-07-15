93b00c2ec363789d2ad42b5273ac893792d52640
Abhay <abhaysingbhosale1@gmail.com>
2021-07-15 23:02:23 +0530

    added new method to download video at provided destination (Beta)

--- InstagramVideosDownloader/src/main/java/com/hcr2bot/instagramvideosdownloader/InstaVideo.java
+++ InstagramVideosDownloader/src/main/java/com/hcr2bot/instagramvideosdownloader/InstaVideo.java
@@ -87,4 +87,75 @@ public class InstaVideo {
 
 
     }
+
+    public static void downloadVideo(Context context, String postUrl, String directory) {
+
+        String replacedUrl;
+        final String[] finalVideoUrl = new String[1];
+
+        RequestQueue requestQueue;
+        requestQueue = Volley.newRequestQueue(context);
+
+        if (TextUtils.isEmpty(postUrl)) {
+            Log.e("VideoURLErrors", "Provided String is empty.");
+        } else {
+            if (postUrl.contains("?utm_source=ig_web_copy_link")) {
+                String partToRemove = "?utm_source=ig_web_copy_link";
+                replacedUrl = postUrl.replace(partToRemove, "");
+            } else if (postUrl.contains("?utm_source=ig_web_button_share_sheet")) {
+                String partToRemove = "?utm_source=ig_web_button_share_sheet";
+                replacedUrl = postUrl.replace(partToRemove, "");
+            } else if (postUrl.contains("?utm_medium=share_sheet")) {
+                String partToRemove = "?utm_medium=share_sheet";
+                replacedUrl = postUrl.replace(partToRemove, "");
+            } else if (postUrl.contains("?utm_medium=copy_link")) {
+                String partToRemove = "?utm_medium=copy_link";
+                replacedUrl = postUrl.replace(partToRemove, "");
+            } else {
+                replacedUrl = postUrl;
+            }
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                    replacedUrl + "?__a=1", null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+
+                    JSONObject Obj1 = null;
+                    try {
+                        Obj1 = response.getJSONObject("graphql");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    JSONObject Obj2 = null;
+                    try {
+                        Obj2 = Obj1.getJSONObject("shortcode_media");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        finalVideoUrl[0] = Obj2.getString("video_url");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d("finalURL", finalVideoUrl[0]);
+                    Util.download(finalVideoUrl[0], "/Insta Video Downloader/" + directory + "/", context, System.currentTimeMillis() + ".mp4");
+
+
+                }
+
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("VideoURLErrors", "Something went wrong" + error);
+                    Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+            requestQueue.add(jsonObjectRequest);
+
+        }
+
+
+    }
 }
